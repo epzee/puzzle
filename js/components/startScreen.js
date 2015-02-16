b9ac0a2fe03c3cb11abd28773a75bea0bcacedff
Samuel Molinari <samuel@molinari.me>
2015-02-16 13:13:35 +0000

    Update button names

--- js/components/startScreen.js
+++ js/components/startScreen.js
@@ -1,8 +1,8 @@
 var StartScreen = React.createClass({
   render: function () {
     var button;
-    var START_BUTTON = <button className="btn btn-success" onClick={this.props.handleStart}>START !!!</button>;
-    var END_BUTTON   = <button className="btn btn-danger" onClick={this.props.handleReset}>RESET !!!</button>
+    var START_BUTTON = <button className="btn btn-success" onClick={this.props.handleStart}>Start</button>;
+    var END_BUTTON   = <button className="btn btn-danger" onClick={this.props.handleReset}>Reset</button>
 
     if(!this.props.isPlaying) {
       button = START_BUTTON;
