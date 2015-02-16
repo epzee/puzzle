e6453fefa9f42d49dfc7c996f8eb02d5b6c16651
gu <augusto.mazza@sainsburysentertainment.co.uk>
2015-02-16 11:22:40 +0000

    react + jsx working

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var PuzzleApp = React.createClass({
+  render: function () {
+    //timer
+    //overlay
+    //imgGrid
+    //terminal (parser + feedback)
+    return (<h1>HELOO</h1>);
+  }
+});
+
+React.render(<PuzzleApp/>, document.getElementById('main'));
