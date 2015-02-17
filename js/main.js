f4c3c02b5228ff26271bf4fc6cd2a45209cede24
Samuel Molinari <samuel@molinari.me>
2015-02-17 11:38:46 +0000

    Update

--- js/main.js
+++ js/main.js
@@ -17,6 +17,40 @@ var PuzzleApp = React.createClass({
     return flatGrid[index];
   },
 
+  handleMoveCell: function (currentMove) {
+    // Pre-condition: validate cell and direction syntax before calling this method
+
+    if (!currentMove || !currentMove.cell || !currentMove.direction) {
+      throw Error('Please provide a valid move object');
+    }
+
+    // cell 9 is always empty space (for now)
+    if (currentMove.cell === 'nine') {
+      throw Error('Can\'t move the empty cell');
+    }
+
+    var directionMap = {
+      up: 'moveUp',
+      down: 'moveDown',
+      left: 'moveLeft',
+      right: 'moveRight'
+    };
+
+    var cellMap = {
+      one: 1,
+      two: 2,
+      three: 3,
+      four: 4,
+      five: 5,
+      six: 6,
+      seven: 7,
+      eight: 8,
+      nine: 9
+    };
+
+    this[directionMap[currentMove.direction]](cellMap[currentMove.cell]);
+  },
+
   _move: function _move(cellIndex, rowOffset, colOffset) {
     cellIndex--;
     var flatGrid = _.flatten(this.state.grid);
@@ -93,9 +127,8 @@ var PuzzleApp = React.createClass({
       <div>
         <StartScreen isPlaying={this.state.isPlaying} handleStart={this.startHandler} handleReset={this.resetHandler} />
         <Timer elapsedTime={this.state.elapsedTime} />
-        <Terminal/>
-        <JigsawGrid />
         <JigsawGrid isPlaying={this.state.isPlaying} grid={this.state.grid} />
+        <Terminal/>
       </div>
     );
   }
