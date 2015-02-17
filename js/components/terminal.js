747e8af4b128d27a4279dc3c5e97538b7c8aad55
gu <augusto.mazza@sainsburysentertainment.co.uk>
2015-02-17 10:40:26 +0000

    terminal working

--- js/components/terminal.js
+++ js/components/terminal.js
@@ -10,6 +10,7 @@ var Terminal = React.createClass({
 
     if (e.keyCode === 13) {
       if (PuzzleParser.isValidCommand(this.state.value)) {
+        //todo make child component and remove from state, use props
         this.state.commands.push(<li>{this.state.value}</li>);
         this.setState({value: '', syntaxError: false});
       } else {
@@ -35,11 +36,14 @@ var Terminal = React.createClass({
             {this.state.commands}
           </ul>
         </div>
+        <div className="col-xs-6">
+          <input type="text" placeholder="Type your move here!"
+            value={this.state.value}
+            onKeyDown={this.keyHandler}
+            onChange={this.handleChange}>
+          </input>
+        </div>
         <span className={classes}>Syntax error!</span>
-        <input className="col-xs-6" type="text" placeholder="Type your move here!"
-          value={this.state.value}
-          onKeyDown={this.keyHandler}
-          onChange={this.handleChange}></input>
       </div>
     );
   }
