b7e91b9c0bd73966f0b27cc8c7a903df25dcde14
gu <augusto.mazza@sainsburysentertainment.co.uk>
2015-02-18 11:50:19 +0000

    changed numbers to words in each cell

--- js/components/jigsawGridCell.js
+++ js/components/jigsawGridCell.js
@@ -1,8 +1,23 @@
 var JigsawGridCell = React.createClass({
+  getCellName: function (index) {
+    var names = {
+      1: 'one',
+      2: 'two',
+      3: 'three',
+      4: 'four',
+      5: 'five',
+      6: 'six',
+      7: 'seven',
+      8: 'eight',
+      9: 'nine'
+    };
+
+    return names[index];
+  },
   render: function () {
     return (
       <div className={'jigsaw-grid-cell col-xs-4 jigsaw-grid-cell-' + this.props.cellPosition + ' jigsaw-grid-cell-view-' + this.props.cellIndex}>
-        <div className="label">{this.props.cellIndex}</div>
+        <div className="label">{this.getCellName(this.props.cellIndex)}</div>
       </div>
     );
   }
